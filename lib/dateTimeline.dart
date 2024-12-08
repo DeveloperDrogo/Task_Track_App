5a4a506c08e9ef7ba6838622fb847872e9043be8
Ankith <122361217+AnkithParroPhins@users.noreply.github.com>
2024-12-08 20:02:10 +0530

    update

---
+++ lib/dateTimeline.dart
@@ -0,0 +1,23 @@
+
+// import 'package:flutter/material.dart';
+
+// class DateTimeLine extends StatefulWidget {
+//   const DateTimeLine({super.key});
+
+//   @override
+//   State<DateTimeLine> createState() => _DateTimeLineState();
+// }
+
+// class _DateTimeLineState extends State<DateTimeLine> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return EasyDateTimeLinePicker(
+//   firstDate: DateTime(2025, 1, 1),
+//   lastDate: DateTime(2030, 3, 18),
+//   focusedDate: DateTime(2025, 6, 15),
+//   onDateChange: (date) {
+//     // Handle the selected date.
+//   },
+// );;
+//   }
+// }
